b4bbcb734ff7382fcaed2ae3b26fe729f0225a0b
fizzara <fizzaahmed2000@hotmail.com>
2023-08-23 15:51:59 -0400

    file cleanup

--- tts.py
+++ tts.py
@@ -1,4 +1,3 @@
-import pyttsx3
 from playsound import playsound as ps
 import os
 from gtts import gTTS
@@ -6,9 +5,6 @@ import time
 
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
-# engine = pyttsx3.init() 
-# engine.setProperty("volume", 1.0)
 
 def speak(text, filename="out.mp3"): #take in text to speak and a filename to save it under
     if filename[-4:] != ".mp3": #so i don't have to add .mp3 to the end of every file
@@ -23,20 +19,13 @@ def speak(text, filename="out.mp3"): #take in text to speak and a filename to sa
     try:
         audio.save(f"sounds/{filename}") #save converted audio
     except(AssertionError):
-        return None
+        pass
 
     ps(f"sounds/{filename}", False)
     
-    # engine.say(text)
-    # engine.runAndWait()
-    # engine.stop()
     
 def read(img): #take in image to read
     text = pytesseract.image_to_string((img), lang="eng.undertale") #use tesseract to convert image text to string
     if len(text) > 0 and text[0] == "*": #remove asterisk at start of text
         text = text[1:]
     return text
-
-# ps("sounds/filename.mp3")
-# speak(read("text.png"))
-
